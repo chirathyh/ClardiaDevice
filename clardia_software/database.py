754e72430e863658abc0c2fcc6b36735019c1695
Chirath <chirathyh@gmail.com>
2017-03-22 19:51:03 +0530

    Adding data base parameters

--- clardia_software/database.py
+++ clardia_software/database.py
@@ -1,33 +1,44 @@
-#!/usr/bin/python
+# Clardia - Comprehensive Family Health Assistant , copyright (c) Olivescript < olivescript.com>
+# by Chirath Hettiarachchi
+#The database management of the clardia software
 
+## Database Details of clardia
+USER_NAME = "chiru"
+PASSWORD = "chiru"
+DATABASE = "clardia_test"
+TABLE = "patient_info"
+
+#Database fiels
+# ID
+# Date
+# Gender
+# Age
+# Height
+# BloodGroup
+# Diabetics
+# HighBloodPressure
+# Cholestrol
+# Kidney
+# OtherDiseases
+# Weight(kg)
+# Temp(c)
+# Spo2(%)
+# HeartRate
+# IR
+# RED
+
+#!/usr/bin/python
 import MySQLdb
 
-field = "ID"
-x = 12142
 
 
-# Open database connection
-db = MySQLdb.connect("127.0.0.1","chiru","chiru","clardia_test" )
-#db=MySQLdb.connect(user="root",passwd="",db="clardia_test",unix_socket="/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock")
-# prepare a cursor object using cursor() method
+db = MySQLdb.connect("127.0.0.1","Admin","admin","clardia_test" )
 cursor = db.cursor()
 sql = """INSERT INTO patient_info(ID)
-         VALUES (12121)"""
+         VALUES (33333)"""
 try:
-   # Execute the SQL command
    cursor.execute(sql)
-   # Commit your changes in the database
    db.commit()
 except:
-   # Rollback in case there is any error
    db.rollback()
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
-
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-
-print "Database version : %s " % data
-
-# disconnect from server
 db.close()
